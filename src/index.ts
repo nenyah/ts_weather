c76045320de255da0b8ec9c575b289c4610dec08
steven <steven@nenyah.com>
2020-06-01 14:56:19 +0800

    add commander

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import colors from "colors";
+import commander from "commander";
+
+const command = commander
+  .version("0.1.0")
+  .option("-c,--city [name]", "Add city name")
+  .parse(process.argv);
+if (process.argv.slice(2).length===0) {
+    command.outputHelp(colors.red)
+    process.exit()
+}
+
